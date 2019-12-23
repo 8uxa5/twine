a16c8f420e32b3a08f9f117167f33abb0fbbf61c
Darius Maciunas <dmaciunas@outlook.com>
2019-12-23 10:27:00 +0000

    before sandbox.create() deprecation fix

--- test/lib/credential-manager.js
+++ test/lib/credential-manager.js
@@ -9,23 +9,23 @@ const CredentialManager = require('../../lib/credential-manager');
 chai.use(chaiAsPromised);
 chai.use(dirtyChai);
 
-describe('a credential manager', () => {
+describe('The credential manager', () => {
 	var creds;
 	before(() => {
 		creds = new CredentialManager('twine-test');
 	});
 	it('should return credentials then they are found', async () => {
-		await creds.storeKeyAndSercret('foo', 'bar');
+		await creds.storeKeyAndSercret('apiKey', 'foo', 'bar');
 		let [
 			key,
 			secret
-		] = await creds.getKeyAndSecret();
+		] = await creds.getKeyAndSecret('apiKey');
 		expect(key).to.equal('foo');
 		expect(secret).to.equal('bar');
 	});
 	it('should reject when no credentials are found', async () => {
-		await creds.clearKeyAndSecret();
-		expect(creds.getKeyAndSecret()).to.be.rejected();
+		await creds.clearKeyAndSecret('apiKey');
+		expect(creds.getKeyAndSecret('apiKey')).to.be.rejected();
 	});
 	after((done) => {
 		fs.unlink(path.join(process.env.HOME, '.config', 'configstore', 'twine-test.json'), done);
---
+++ test/lib/util.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const expect = chai.expect;
+const dirtyChai = require('dirty-chai');
+const util = require('../../lib/util');
+
+chai.use(dirtyChai);
+
+describe('the util module', () => {
+	context('the not empty function', () => {
+		it('should return true when given string', () => {
+			expect(util.notEmpty('foo')).to.be.true();
+		});
+		it('should return an error when given empty string', () => {
+			expect(util.notEmpty('')).to.equal('This value is required');
+		});
+	});
+});
