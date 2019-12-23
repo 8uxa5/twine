26ea5b9c3cb6ff2635b96cf22b759bd04ea5a615
Darius Maciunas <dmaciunas@outlook.com>
2019-12-23 18:50:22 +0000

    hopefully final commit

--- bin/twine.js
+++ bin/twine.js
@@ -3,4 +3,8 @@
 const program = require('commander');
 const pkg = require('../package.json');
 
-program.version(pkg.version).command('configure', 'configure Twitter-related credentials').parse(process.argv);
+program
+	.version(pkg.version)
+	.command('configure', 'configure Twitter-related credentials')
+	.command('lookup', 'lookup things on Twitter')
+	.parse(process.argv);
--- test/lib/credential-manager.js
+++ test/lib/credential-manager.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const fs = require('fs-extra');
+const sinon = require('sinon');
+const keytar = require('keytar');
+const _ = require('lodash');
 const chai = require('chai');
 const expect = chai.expect;
 const chaiAsPromised = require('chai-as-promised');
@@ -10,12 +13,49 @@ chai.use(chaiAsPromised);
 chai.use(dirtyChai);
 
 describe('The credential manager', () => {
+	var secrets = {};
 	var creds;
 	before(() => {
+		sinon.stub(keytar, 'setPassword').callsFake((service, key, secret) => {
+			_.set(secrets, `${service}.${key}`, secret);
+			return Promise.resolve();
+		});
+		sinon.stub(keytar, 'getPassword').callsFake((service, key) => {
+			let value = _.get(secrets, `${service}.${key}`);
+			return value ? Promise.resolve(value) : Promise.reject(new Error('Missing consumer secret'));
+		});
+		sinon.stub(keytar, 'deletePassword').callsFake((service, key) => {
+			_.unset(secrets, `${service}.${key}`);
+			return Promise.resolve();
+		});
+
 		creds = new CredentialManager('twine-test');
 	});
 
-	it('should return credentials then they are found', async () => {
+	it('should RETURN credentials set in the enviroment', async () => {
+		process.env['TWINE-TEST_CONSUMER_KEY'] = 'one';
+		process.env['TWINE-TEST_CONSUMER_SECRET'] = 'two';
+		let [
+			key,
+			secret
+		] = await creds.getKeyAndSecret('consumer');
+		expect(key).to.equal('one');
+		expect(secret).to.equal('two');
+	});
+
+	it('should PREFER credentials set in the enviroment', async () => {
+		await creds.storeKeyAndSercret('consumer', 'foo', 'bar');
+		let [
+			key,
+			secret
+		] = await creds.getKeyAndSecret('consumer');
+		expect(key).to.equal('one');
+		expect(secret).to.equal('two');
+		delete process.env['TWINE-TEST_CONSUMER_KEY'];
+		delete process.env['TWINE-TEST_CONSUMER_SECRET'];
+	});
+
+	it('should RETURN credentials then they are found', async () => {
 		await creds.storeKeyAndSercret('consumer', 'foo', 'bar');
 		let [
 			key,
@@ -25,18 +65,18 @@ describe('The credential manager', () => {
 		expect(secret).to.equal('bar');
 	});
 
-	it('should reject when no key are found', async () => {
+	it('should reject when NO KEY are found', async () => {
 		await creds.clearKeyAndSecret('consumer');
 		expect(creds.getKeyAndSecret('consumer')).to.be.rejectedWith('Missing consumer key');
 	});
 
-	it('should reject when no secret is found', async () => {
+	it('should reject when NO SECRET is found', async () => {
 		creds.conf.set('keys.consumer', 'foo');
 		await expect(creds.getKeyAndSecret('consumer')).to.be.rejectedWith('Missing consumer secret');
 		creds.conf.delete('keys.consumer');
 	});
 
-	it('should remove all credentials', async () => {
+	it('should REMOVE all credentials', async () => {
 		await creds.storeKeyAndSercret('consumer', 'one', 'two');
 		await creds.storeKeyAndSercret('account', 'three', 'four');
 		await creds.clearAll();
@@ -46,6 +86,9 @@ describe('The credential manager', () => {
 
 	after(async () => {
 		await creds.clearAll();
+		keytar.setPassword.restore();
+		keytar.getPassword.restore();
+		keytar.deletePassword.restore();
 		await fs.unlink(path.join(process.env.HOME, '.config', 'configstore', 'twine-test.json'));
 	});
 });
